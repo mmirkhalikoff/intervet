c4a5db7735937b7442b6a069f883c6e4a8d5db4c
Mirjalol <mirjalol.mirm.m@gmail.com>
2020-10-18 18:34:57 +0500

    animal

---
+++ js/modal.js
@@ -0,0 +1,14 @@
+// const modal = document.getElementById('modal')
+
+// function openPartnerModal(avatar, name, firstname, lastname, text) {
+//     modal.style.display = 'block'
+//     modal.getElementsByClassName('avatar')[0].src = avatar
+//     modal.getElementsByClassName('name')[0].innerHTML = name
+//     modal.getElementsByClassName('firstname')[0].innerHTML = firstname
+//     modal.getElementsByClassName('lastname')[0].innerHTML = lastname
+//     modal.getElementsByClassName('text')[0].innerHTML = text
+// }
+
+// function closeModal() {
+//     modal.style.display = 'none'
+// }
